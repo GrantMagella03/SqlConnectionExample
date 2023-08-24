2fc181f8ed560120ff56a404987684aed9881483
Grant Magella <Bitman2003@gmail.com>
2023-08-24 09:00:06 -0400

    Add project files.

---
+++ SqlConnectionExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Data.SqlClient;
+
+internal class Program {
+    private static void Main(string[] args) {
+        var connStr = "server=localhost\\sqlexpress;" +
+            "database=SalesDb;" +
+            "trusted_connection=true;" +
+            "trustServerCertificate=true;";
+        var conn = new SqlConnection(connStr);
+        conn.Open();
+        if (conn.State != System.Data.ConnectionState.Open) {
+            throw new Exception("Connection didn't open");
+        }
+        Console.WriteLine("Success");
+        var sql = "SELECT * FROM Customers WHERE id BETWEEN 10 AND 19;";
+        var cmd = new SqlCommand(sql, conn);
+        var reader = cmd.ExecuteReader();
+        //reader.Read();//returns true if reader has more data to process
+        while(reader.Read()) {
+            var id = (int)reader["Id"];
+            var name = (string)reader["Name"];
+            Console.WriteLine($"{id} {name}");
+        }
+        conn.Close();
+    }
+}
